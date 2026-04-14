01c80d74fcbe3532ed8ece177233949068f27abb
Rahul Kumar <rahulk49855@gmail.com>
2026-04-14 18:33:28 +0530

    Fixed CORS issue

--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,12 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "https://mern-project-qmqa5t11h-rahulk49855-2371s-projects.vercel.app", // ✅ ADD THIS
       "https://mern-project-qie26haah-rahulk49855-2371s-projects.vercel.app",
       "https://mern-project-tau-eosin.vercel.app",
       "https://mern-project-mzinywk5n-rahulk49855-2371s-projects.vercel.app",
     ],
+    credentials: true,
   })
 );
 app.use(express.json());
