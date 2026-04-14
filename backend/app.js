c9f331d2c9efe476a8f6736c2899b22d799098f6
Rahul Kumar <rahulk49855@gmail.com>
2026-04-14 18:03:17 +0530

    final cors fix

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const authRoutes = require('./routes/authRoutes');
-const courseRoutes = require('./routes/courseRoutes');
-const purchaseRoutes = require('./routes/purchaseRoutes'); // added
+const authRoutes = require("./routes/authRoutes");
+const courseRoutes = require("./routes/courseRoutes");
+const purchaseRoutes = require("./routes/purchaseRoutes"); // added
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "https://mern-project-qie26haah-rahulk49855-2371s-projects.vercel.app",
-    "https://mern-project-tau-eosin.vercel.app"
-  ]
-}));
-
+app.use(
+  cors({
+    origin: [
+      "https://mern-project-qie26haah-rahulk49855-2371s-projects.vercel.app",
+      "https://mern-project-tau-eosin.vercel.app",
+      "https://mern-project-mzinywk5n-rahulk49855-2371s-projects.vercel.app",
+    ],
+  })
+);
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/courses', courseRoutes);
-app.use('/api/purchase', purchaseRoutes); // added
+app.use("/api/auth", authRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/purchase", purchaseRoutes); // added
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("Course Management API Running");
 });
 
