f1b21ee7d725a5f6b9676eed1bcfdb080fc209df
Rahul Kumar <rahulk49855@gmail.com>
2026-04-15 13:49:06 +0530

    updated code

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // IMPORTANT for preflight requests
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 
